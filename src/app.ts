e64d5cecba0231bd9e797993b14626ce64f975fb
motemed-khaled <motemedkhaled5@gmail.com>
2024-09-24 12:44:51 +0300

    update client routing serve

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ mountRoutes(app);
 app.use(express.static(path.join(__dirname, '../client', 'build')));
 
 
-app.get('/admin/*', (req, res) => {
+app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname , '../client', 'build', 'index.html'));
 });
 
